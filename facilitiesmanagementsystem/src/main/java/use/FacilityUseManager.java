5a2b6df965482512f6c3cc9de890a0f9d4a10569
adelaney11 <adam.delaney11@gmail.com>
2020-02-23 18:36:05 -0600

    Facility Use  interface partially completed

--- facilitiesmanagementsystem/src/main/java/use/FacilityUseManager.java
+++ facilitiesmanagementsystem/src/main/java/use/FacilityUseManager.java
@@ -1,30 +1,57 @@
 package use;
 
+import java.util.*;
+import facility.*;
+
 public class FacilityUseManager implements FacilityUse {
+    protected ArrayList<User> userList;
+    private int userCount = 0;
 
+    public boolean isInUseDuringInterval(Facility f, String day){
+        
+        // check if userList date is already taken
+        
+        for (int i = 0; i<userList.size(); i++){
+            if (userList.get(i).dayUsing == day) {
+                return true;
+            }
+            else {
+                continue;
+            }
+        }
+        return false;
+    }
 
+    public void assignFacilityToUse(Facility f, User u){
+        
+        // assign a facility to a user
+        
+        u.setFacilityID(f.getFacilityID());
+        userList.add(u);
+    }
 
-    public boolean isInUseDuringInterval(Facility f, ){
+    public void vacateFacility(Facility f, String day){
+        
+        // check if a facility is assigned to be used for a certain time,
+        // if so, remove that assignment
 
     }
 
-    public void assignFacilityToUse(){
+    public String[] listInspections(Facility f){
+        
+        // unsure what this should do
 
     }
 
-    public void vacateFacility(){
+    public void listActualUsage(Facility f){
+
+        // list total time a facility has been used
 
     }
 
-    public String[] listInspections(){
+    public int calcUsageRate(Facility f){
 
-    }
-
-    public void listActualUsage(){
-
-    }
-
-    public int calcUsageRate(){
+        // list total time a facility has been used / time unused during business hours (?)
 
     }
 }
