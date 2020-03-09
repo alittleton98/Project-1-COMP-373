f4cbdd8461b897cff82ffd082574075106987312
adelaney11 <adam.delaney11@gmail.com>
2020-03-09 17:32:00 -0500

    junit dependency in xml

--- facilitiesmanagementsystem/src/test/java/facilitytest/FacilityManagerTest.java
+++ facilitiesmanagementsystem/src/test/java/facilitytest/FacilityManagerTest.java
@@ -3,26 +3,27 @@ import org.junit.*;
 import facility.*;
 
 public class FacilityManagerTest{
-    Facility testFacility;
-
+    FacilityManager testFacilityManager = new FacilityManager();
+    Facility testFacility = new Facility();
+    
     @Test
     public void listAvailableFacilitiesTest(){
-
+        
     }
 
     @Test
     public void getFacilityInformationTest(Facility f){
-
+        
     }
 
     @Test
     public int requestAvailableCapacityTest(Facility f){
-
+        
     }
 
     @Test
     public void addNewFacilityTest(Facility f){
-
+        testFacilityManager.addNewFacility(f);
     }
 
     @Test
