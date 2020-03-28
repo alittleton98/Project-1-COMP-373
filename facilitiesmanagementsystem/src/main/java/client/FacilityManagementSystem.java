e9cb42513c0c1f285b1d1919fa0cee2f3bab65f7
adelaney11 <adam.delaney11@gmail.com>
2020-03-28 13:39:33 -0500

    Begin implementing dependency injection w/ Spring

--- facilitiesmanagementsystem/src/main/java/client/FacilityManagementSystem.java
+++ facilitiesmanagementsystem/src/main/java/client/FacilityManagementSystem.java
@@ -1,11 +1,18 @@
 package client;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import facility.*;
 import maintenance.*;
 import use.*;
 
 public class FacilityManagementSystem {
     public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/app-context.xml");
+	    System.out.println("***************** Application Context instantiated! ******************");
 
     }
 }
